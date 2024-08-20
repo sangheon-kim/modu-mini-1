793d6877218cd0b56b3ce368b6f55a13fff4a5f1
sangheon-kim <ksj83671@gmail.com>
2024-08-20 14:44:55 +0900

    [김상헌] 타임라인

--- src/todo.ts
+++ src/todo.ts
@@ -1,2 +1,11 @@
 import "./main";
 import "./styles/todo.scss";
+import dayjs from "dayjs";
+
+const timelines = new Array(48).fill(0).map((_, i) => {
+  return dayjs(new Date("2021-07-01T00:00:00"))
+    .add(i * 30, "minute")
+    .format("HH:mm");
+});
+
+console.log(timelines);
